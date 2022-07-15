248a10ac48c6f083805de9b9db430b2a05134f6f
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-14 23:16:42 -0500

    Return serialized person upon GET request to \user
    Modify UserServlet.doGet to return a person object serialized in JSON
    when a GET request is made to the \user path. The Person class provided
    in the models package the ORM .jar file is used to construct the person
    object. The Jackson library is used to convert the java object to the
    JSON string.

--- src/main/java/com/revature/versusapp/servlets/UserServlet.java
+++ src/main/java/com/revature/versusapp/servlets/UserServlet.java
@@ -8,6 +8,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.revature.versusapp.models.Person;
+
 //our servlets should extend HttpServlet
 public class UserServlet extends HttpServlet {
      // the servlet container (tomcat catalina) will call this method when
@@ -17,6 +22,25 @@ public class UserServlet extends HttpServlet {
      protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
          // gets the response body writer object so that we can write to the response body
          PrintWriter writer = resp.getWriter();
-         writer.write("I have made a few changes. MyWebApp says hello! :)");
+         
+         Person person = new Person("apiuser","1234","Api","User");
+         ObjectMapper objMapper = new ObjectMapper();
+         
+         String serializedPerson = null;
+         try {
+             //System.out.println(objMapper.writeValueAsString(user));
+             serializedPerson = objMapper.writeValueAsString(person);
+         } catch (JsonProcessingException e) {
+             e.printStackTrace();
+         }
+         
+         if ( serializedPerson != null ) {
+             writer.write(serializedPerson);
+         }
+         else {
+             writer.write("Serialization has failed.\n");
+         }
+         
+         
      }
 }
