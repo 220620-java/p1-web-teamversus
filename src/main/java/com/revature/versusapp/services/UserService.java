1f3c0c34b02ac6cac056ad4815be8f2cf27a41c1
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-15 17:47:33 -0500

    Add a sample test case for UserService

--- src/main/java/com/revature/versusapp/services/UserService.java
+++ src/main/java/com/revature/versusapp/services/UserService.java
@@ -1,5 +1,22 @@
 package com.revature.versusapp.services;
 
+import com.revature.versusapp.data.ORM;
+import com.revature.versusapp.models.Person;
+
 public class UserService { 
+    private ORM dbORM;
     
+    UserService() {
+        dbORM = new ORM();
+    }
+    
+    public Person register(String username, String password) {
+        //This is totally wrong and needs to be made to work.
+        Person newPerson = new Person();
+        newPerson.setId(3);
+        
+        newPerson = (Person) dbORM.findById(newPerson);
+        
+        return newPerson;
+    }
 }
--- src/test/java/com/revature/versusapp/services/UserServiceTest.java
+++ src/test/java/com/revature/versusapp/services/UserServiceTest.java
@@ -1,14 +1,40 @@
 package com.revature.versusapp.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.revature.versusapp.data.ORM;
+import com.revature.versusapp.models.Person;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+    @InjectMocks
+    private UserService userServ = new UserService();
+    
+    @Mock
+    private ORM dbORM;
+    
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testRegisterValidUser() {
+        Person mockPerson = new Person("username","password","","");
+
+        // Ensure that the ORM will give us a nonnull person objcet.
+        Mockito.when(dbORM.findById(any(Person.class))).thenReturn(mockPerson);
+        
+        Person returnedPerson  = userServ.register("username","password");
+        
+        // assertion
+        assertNotNull(returnedPerson);
     }
+    
+    
 
 }
