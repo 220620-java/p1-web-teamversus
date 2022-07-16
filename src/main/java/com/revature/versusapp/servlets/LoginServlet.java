05ac643189d83256ad3217f07469cb9b65892e6a
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-16 18:03:55 -0500

    Fix capilalization and comments in web.xml

--- src/main/java/com/revature/versusapp/servlets/LoginServlet.java
+++ src/main/java/com/revature/versusapp/servlets/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.revature.versusapp.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -10,8 +11,9 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doPost(req, resp);
+        PrintWriter writer = resp.getWriter();
+        
+        writer.write("You have posted to the login servlet. congrats.");
     }
     
     
