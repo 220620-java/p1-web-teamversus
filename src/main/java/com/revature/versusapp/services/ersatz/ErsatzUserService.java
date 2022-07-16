4eb0148b434e81702dfa14cd64bcca0915d1292e
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-16 17:22:40 -0500

    Add ersatz user service files

---
+++ src/main/java/com/revature/versusapp/services/ersatz/ErsatzUserService.java
@@ -0,0 +1,44 @@
+package com.revature.versusapp.services.ersatz;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.versusapp.models.Artist;
+import com.revature.versusapp.utils.ConnectionUtil;
+
+public class ErsatzUserService {
+    private ConnectionUtil connUtil;
+    
+    public ErsatzUserService() {
+        connUtil = ConnectionUtil.getConnectionUtil();
+    }
+    
+    public List<Artist> GetArtists() {
+        List<Artist> artists = new ArrayList<>();
+        
+        try (Connection connection = connUtil.getConnection()) {
+
+            Statement selectStmt = connection.createStatement();
+            ResultSet rs = selectStmt
+                .executeQuery("select id, stage_name from artist;");
+            
+            while(rs.next())
+            {
+               int id = rs.getInt("id");
+               String stageName = rs.getString("stage_name");
+               
+               Artist artist = new Artist(id,stageName);
+               artists.add(artist);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return artists;
+    }
+}
