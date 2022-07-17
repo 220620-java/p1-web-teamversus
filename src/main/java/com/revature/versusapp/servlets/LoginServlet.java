54f61ca98b2df8b74cef7469677f951fe0186927
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-16 19:56:40 -0500

    Deserialize request body in Login servlet

--- src/main/java/com/revature/versusapp/servlets/LoginServlet.java
+++ src/main/java/com/revature/versusapp/servlets/LoginServlet.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.versusapp.models.rest.Credentials;
 import com.revature.versusapp.models.rest.Login;
+import com.revature.versusapp.utils.ObjectMapperUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -17,30 +19,40 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
         
-        String payload  = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        ObjectMapper objMapper = ObjectMapperUtil.getObjectMapper();
         
-        writer.write("You have posted to the login servlet. congrats.");
-        ObjectMapper objMapper = new ObjectMapper();
+        // Attempt to deserialize the request body as a Login object. Return
+        // SC_BAD_REQUEST if unable to do so.
+        String body = req.getReader()
+                         .lines()
+                         .collect(Collectors.joining(System.lineSeparator()));
+        boolean hasError = false;
+        String errorString = "";
+        Login login = null;
         
         try {
-            Login car =  objMapper.readValue(payload, Login.class);
-            
-            writer.write("login deserialized\n");
-            writer.write("user name: " + car.getUsername() + "\n");
-            writer.write("password: " + car.getPassword() + "\n");
-            
+            login = objMapper.readValue(body, Login.class);
         } catch (JsonMappingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            hasError = true;
+            errorString = e.getMessage();
         } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            hasError = true;
+            errorString = e.getMessage();
         }
         
+        if ( hasError ) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write(errorString);
+            return;
+        }
         
+        Credentials credentials = new Credentials(1,1);
+        String serialCredential = objMapper.writeValueAsString(credentials);
+
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        resp.setContentLength(serialCredential.length());
+        resp.getWriter().write(serialCredential);
     }
-    
-    
 }
