cd11fbcc21ce2ec96056b78aff0865a2154604a8
evgeniyko99 <ekk4357@gmail.com>
2022-07-16 22:33:32 -0400

    UserService login()

--- src/main/java/com/revature/versusapp/services/UserService.java
+++ src/main/java/com/revature/versusapp/services/UserService.java
@@ -1,5 +1,8 @@
 package com.revature.versusapp.services;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import com.revature.versusapp.data.ORM;
 import com.revature.versusapp.models.Person;
 
@@ -8,6 +11,45 @@ public class UserService {
     
     UserService() {
         dbORM = new ORM();
+    }
+    
+    public Person login(String username, String password) {
+    	Person person = new Person();
+    	List<Object> allAccounts = dbORM.findAll(person);
+    	Field field;
+    	Boolean found = false;
+		try {
+			field = person.getClass().getDeclaredField("username");
+			for (Object object : allAccounts) {
+	    		if (field.get(object).equals(username)) {
+	    			Field passwordField = person.getClass().getDeclaredField("passwrd");
+	    			if(passwordField.get(object).equals(password)) {
+	    				found = true;
+		    			person = (Person) object;
+	    			} else {
+	    				System.out.println("Password is incorrect");
+	    				return null;
+	    			}
+	    		}
+	    	}
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (found) {
+			System.out.println("username not found");
+			return null;
+		}
+    	return person;
     }
     
     public Person register(String username, String password) {
