df864dbb6b14f9557cecaae1375f76176cfc5fbf
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-17 00:46:25 -0500

    Add NewArtist rest model

---
+++ src/main/java/com/revature/versusapp/models/rest/NewArtist.java
@@ -0,0 +1,13 @@
+package com.revature.versusapp.models.rest;
+
+public class NewArtist {
+    private String stageName;
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public void setStageName(String stageName) {
+        this.stageName = stageName;
+    }
+}
