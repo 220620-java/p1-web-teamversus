a246de0d43b40988a50241a5636ecd80d5eb4339
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-18 00:47:20 -0500

    Add fields to Registration REST class

--- src/main/java/com/revature/versusapp/models/rest/Registration.java
+++ src/main/java/com/revature/versusapp/models/rest/Registration.java
@@ -1,5 +1,19 @@
 package com.revature.versusapp.models.rest;
 
-public class Registration {
-
+public class Registration extends Login{
+    private String firstname;
+    private String lastname;
+    
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
