367ab20dd8e271cf5c1743044bf9c1023ca60611
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-17 22:22:46 -0500

    Implement GET method for inventory servlet

--- src/main/java/com/revature/versusapp/services/ersatz/ErsatzInventoryService.java
+++ src/main/java/com/revature/versusapp/services/ersatz/ErsatzInventoryService.java
@@ -1,5 +1,15 @@
 package com.revature.versusapp.services.ersatz;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.versusapp.models.Artist;
+import com.revature.versusapp.models.rest.InventoryItem;
+import com.revature.versusapp.models.rest.NewAlbum;
 import com.revature.versusapp.utils.ConnectionUtil;
 
 public class ErsatzInventoryService {
@@ -9,5 +19,62 @@ public class ErsatzInventoryService {
         connUtil = ConnectionUtil.getConnectionUtil();
     }
     
+    public List<InventoryItem> getInventory(){
+        List<InventoryItem> inventory = new ArrayList<InventoryItem>();
+        
+        try (Connection connection = connUtil.getConnection()) {
+
+            Statement selectStmt = connection.createStatement();
+            ResultSet rs = selectStmt
+                .executeQuery("select username, title, stage_name \r\n"
+                        + "from inventory \r\n"
+                        + "inner join person on inventory.person_id = person.id \r\n"
+                        + "inner join album on inventory.album_id = album.id \r\n"
+                        + "inner join artist on album.artist_id = artist.id \r\n"
+                        + "order by username,stage_name,title;");
+            
+            String username = null;
+            String title = null;
+            String stageName = null;
+            
+            InventoryItem item = new InventoryItem();
+            boolean hasItems = false;
+            
+            while(rs.next())
+            {
+                username = rs.getString("username");
+                title = rs.getString("title");
+                stageName = rs.getString("stage_name");
+                
+                NewAlbum album = new NewAlbum();
+                album.setArtist(stageName);
+                album.setTitle(title);
+                
+                // For the first time through the loop, item.getUsername()
+                // will be null, so set it to username here.
+                if ( item.getUsername() == null ) {
+                    item.setUsername(username);
+                }
+                
+                // If we've reached a new username, put the current item
+                // in the inventory and start a new one.
+                if ( item.getUsername()!= null && item.getUsername().equals(username) ) {
+                    inventory.add(item);
+                    item = new InventoryItem();
+                    item.setUsername(username);
+                }
+                
+                item.getAlbums().add(album);
+            }
+            
+            //Add the last item that was worked on to inventory.
+            inventory.add(item);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return inventory;
+    }
     
 }
--- src/main/java/com/revature/versusapp/servlets/InventoryServlet.java
+++ src/main/java/com/revature/versusapp/servlets/InventoryServlet.java
@@ -1,9 +1,13 @@
 package com.revature.versusapp.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.versusapp.services.ersatz.ErsatzAlbumService;
+import com.revature.versusapp.models.Artist;
+import com.revature.versusapp.models.rest.InventoryItem;
+import com.revature.versusapp.services.ersatz.ErsatzInventoryService;
 import com.revature.versusapp.utils.ObjectMapperUtil;
 
 import jakarta.servlet.ServletException;
@@ -13,15 +17,33 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class InventoryServlet extends HttpServlet{
     private ObjectMapper objMapper;
+    private ErsatzInventoryService inventoryService;
     
     {
         objMapper = ObjectMapperUtil.getObjectMapper();
+        inventoryService = new ErsatzInventoryService();
     }
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doGet(req, resp);
+        List<InventoryItem> items = inventoryService.getInventory();
+        
+        String inventory = null;
+        try {
+            inventory = objMapper.writeValueAsString(items);
+        } catch (JsonProcessingException e) {
+            
+        }
+        
+        if ( inventory != null ) {
+            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType("application/json");
+            resp.getWriter().write(inventory);
+        }
+        else {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write("Serialization has failed.\n");
+        }
     }
     
     @Override
