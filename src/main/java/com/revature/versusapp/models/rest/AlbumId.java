763d8691fed33b440cded6491f451ed914844590
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-20 18:16:57 -0500

    Add additional classes for use with Jackson

---
+++ src/main/java/com/revature/versusapp/models/rest/AlbumId.java
@@ -0,0 +1,15 @@
+package com.revature.versusapp.models.rest;
+
+public class AlbumId {
+    int albumId;
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(int albumId) {
+        this.albumId = albumId;
+    }
+    
+    
+}
---
+++ src/main/java/com/revature/versusapp/models/rest/JsonPerson.java
@@ -0,0 +1,27 @@
+package com.revature.versusapp.models.rest;
+
+public class JsonPerson {
+    private String username;
+    private String firstname;
+    private String lastname;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+}
